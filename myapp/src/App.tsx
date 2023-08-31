f0d5b19be9f7bc6a4da5eb365d06467899ee2e9d
fayff2 <fay_ff2@hotmail.com>
2023-08-31 11:03:55 +0300

    init

---
+++ myapp/src/App.tsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import {Routes , Route} from 'react-router-dom'
+
+function App() {
+
+
+  return (
+    <>
+    <Routes>
+      <Route>
+        
+      </Route>
+    </Routes>
+      
+     
+    </>
+  )
+}
+
+export default App
