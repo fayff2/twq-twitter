d35430a3a0a4ef682ab7f451aadb8b632643973f
Bandar1014 <Bandar1014@gmail.com>
2023-08-31 11:25:23 +0300

    new

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import {Routes , Route} from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
     <Routes>
-      <Route>
+      <Route path='/' element={<Home/>}>
         
       </Route>
     </Routes>
