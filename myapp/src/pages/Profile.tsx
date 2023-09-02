74fdaf78c49f96ff5d2d6d44fe31b6790c3421d4
Bandar1014 <Bandar1014@gmail.com>
2023-09-02 21:03:28 +0300

    push

--- myapp/src/pages/Profile.tsx
+++ myapp/src/pages/Profile.tsx
@@ -18,39 +18,48 @@ function Profile() {
         </div>
         {/* logo and edit button */}
         <div className='flex justify-between'> 
-          <div>
-            <button className='text-xs ml-3 rounded-full border border-slate-400 p-1 mt-1'>تعديل الملف الشخصي</button>
-          </div>
           <div className='border border-slate-400 h-14 w-14 rounded-full mr-3'>
               <img className='rounded-full w-auto h-14' src="./src/assets/react-logo.png" alt="" />
+          </div>
+          <div>
+            <button className='text-xs ml-3 rounded-full border border-slate-400 p-1 mt-1'>تعديل الملف الشخصي</button>
           </div>
         </div>
         {/* information */}
 
-        <div className='flex flex-col items-end mr-3 '>
+        <div className='flex flex-col  mr-3 '>
           <p className='text-sm font-bold'>React</p>
-          <p className='text-xs text-slate-400 mb-1'>@react</p>
-          <div className='flex'>
-            <p className='text-xs text-slate-400 mr-2 mb-1'>انضم في يناير 2013</p>
+          <p className='text-xs text-slate-400 mb-1'>react@</p>
+          <div className='flex gap-2'>
+            <div className='flex'>
+              <img className='w-3 ' src="./src/assets/loc.svg" alt="" />
             <p className='text-xs text-slate-400 mb-1'>
               المملكة العربية السعودية
               </p>
 
+            </div>
+            <div className='flex'>
+              <img className='w-3 sla text-slate-400' src="./src/assets/calender.svg" alt="" />
+            <p className='text-xs text-slate-400  mb-1'>
+              انضم في يناير 2013 
+              </p>
+            </div>
+
           </div>
-          <div className='flex '>
-            <p className='text-xs '>  <span className='text-slate-400 mr-1'> Flowers</span>144</p>
-            <p className='text-xs ml-2'>  <span className='text-slate-400 mr-1'> floweing</span>593</p>
+          <div className='flex mt-2'>
+            <p className='text-xs ml-2'> 593 <span className='text-slate-400'> متابَع</span></p>
+            <p className='text-xs '> 144 <span className='text-slate-400'>متابِع</span></p>
           </div>
         </div>
 
         {/* list */}
 
         <div className='flex gap-5 text-xs justify-center mt-5'>
-          <a href="">الإعجابات</a>
-          <a href="">الوسائط</a>
-          <a href="">المميزة</a>
-          <a href="">الردود</a>
           <a href="">المنشورات</a>
+          <a href="">الردود</a>
+          <a href="">المميزة</a>
+          <a href="">الوسائط</a>
+          <a href="">الإعجابات</a>
         </div>
 
 
